126fba7a7119399efd92d3769cf33ab09f1279b7
kokorokaiya <poooooooul@yahoo.co.jp>
2018-10-26 13:18:40 +0900

    26,10.2018

---
+++ TestDB/sql/testdb_koko.sql
@@ -0,0 +1,14 @@
+drop database if exists testdb_koko;
+create database testdb_koko;
+use testdb_koko;
+
+
+create table test_table(
+No int, name varchar(50), age int, Mail varchar(50));
+
+insert into test_table values(1,"kinoshita", 39, "yyy@jjddddd");
+insert into test_table values(2, "okada", 66, "ggg@hdjhdjdkd");
+insert into test_table values(3, "ishida", 69, "ghdjd@huuhu");
+
+
+
