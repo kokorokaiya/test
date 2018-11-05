97a063dd6130d57934fd02e68494abc91bdf6d6f
kokorokaiya <poooooooul@yahoo.co.jp>
2018-11-05 14:37:04 +0900

    0511

---
+++ WebProj4/src/Com/internousdev/webproj4/action/InquiryAction.java
@@ -0,0 +1,13 @@
+package Com.internousdev.webproj4.action;
+
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+
+public class InquiryAction extends ActionSupport {
+	public String execute() {
+		return SUCCESS;
+	}
+
+}
